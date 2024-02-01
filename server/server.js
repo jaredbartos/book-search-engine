4ed50ebcc455a19ed60877ed7a2f386700b0db55
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2024-02-01 09:50:36 -0600

    Refactor back-end auth.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
