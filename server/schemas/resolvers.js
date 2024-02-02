78cf57d2d0615d6a1ca3cd6007498685cca80fd8
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2024-02-01 20:46:34 -0600

    Add resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,9 +2,72 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 const { User } = require('../models');
 
 const resolvers = {
-  // Query: {
-  //   me: 
-  // }
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id).populate('savedBooks');
+        return user;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    }
+  },
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+
+      return { token, user }
+    },
+    saveBook: async (parent, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: args } },
+          { new: true, runValidators: true }
+        );
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    }
+  }
 }
 
 module.exports = resolvers;
